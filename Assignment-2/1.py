83ee5f4f3abec8e040b232cd04127ccf1e26b2e7
manishhedau <hedaumanish@gmail.com>
2020-07-07 10:38:12 +0530

    second assigment

---
+++ Assignment-2/1.py
@@ -0,0 +1,10 @@
+# Question - 1
+# for i in range(9):
+#     for j in range(5):
+#         if i == j:
+#             print('*'*(j))
+#         # print()
+#     print()
+
+for i in range(5):
+    print((chr(65+i)+' ')*5)
---
+++ Assignment-2/2.py
@@ -0,0 +1,9 @@
+# Question - 2
+input_word = input()
+input_word = reversed(input_word)
+output = ''
+
+for i in input_word:
+    output = output + i
+
+print(output)
